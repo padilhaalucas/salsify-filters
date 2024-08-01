df02bdb541a3b08fbb67f1d5a6f5b2f2ad336397
Lucas Padilha <lucas_padilha@icloud.com>
2024-08-01 16:13:36 +0100

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Add other configurations here as needed
+};
 
-export default nextConfig;
+module.exports = nextConfig;
